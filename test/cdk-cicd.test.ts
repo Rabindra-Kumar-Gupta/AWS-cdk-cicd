897562c8be56d0c62b1d51a2726ea77566dd27d5
rabindra_kumar <rabindra_kumar@epam.com>
2024-02-15 17:38:43 +0530

    break build

--- test/cdk-cicd.test.ts
+++ test/cdk-cicd.test.ts
@@ -1,7 +1,7 @@
 import { handler } from "../services/hello"
 
 describe('hello describe test suite', () =>{
-    test('handler should return 200', async ()=> {
+    test('handler should return 400', async ()=> {
         const result = await handler ({},{})
         expect(result.statusCode).toBe(200)
     })
